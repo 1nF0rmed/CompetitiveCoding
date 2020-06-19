3b1c810439810bfd2ca72ad0d5d127ce0a3b8b1b
1nF0rmed <prkinformed@gmail.com>
2020-06-19 12:39:54 +0530

    Daily commit

---
+++ MISSP.cpp
@@ -0,0 +1,18 @@
+//
+// https://www.codechef.com/problems/MISSP
+//
+#include <stdio.h>
+
+int main(){
+	long a,b,c,i;
+	scanf("%li",&a);
+	while(a--){
+		i=0;
+		scanf("%li",&b);
+		while(b--){
+			scanf("%li",&c);
+			i^=c;
+		}
+		printf("%li\n",i);
+	}
+}
